daca60c2ce847c8da6999a944fd8645111f7e30e
rohithmmm <rohithmreddy1993@gmail.com>
2025-10-07 16:56:33 -0400

    add functionality has been added

--- abc.java
+++ abc.java
@@ -6,5 +6,7 @@ class Main {
     System.out.println("Living in the world");
     System.out.println("Good Bye world");
     System.out.println("gt1-newchange");
+    int a=1+2;
+    System.out.println(a);
   }
 }
